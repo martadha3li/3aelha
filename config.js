815f76aeedec67a3cbaf908daf077a95c1485b64
martadha3li <martadha3li@gmail.com>
2026-02-16 13:51:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBUwWf2f5hNX6HsandPrbem2o4I3cfj1As",
@@ -12,8 +11,6 @@ const firebaseConfig = {
   appId: "1:1082290967333:web:4440c797e1f85d12c700cc"
 };
 
-// تشغيل Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
